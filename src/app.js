cabb32fb23fc38535091561e8d38f0c3340daec4
NToan324 <nhattoan664t@gmail.com>
2024-10-21 22:57:20 +0700

    up:code

--- src/app.js
+++ src/app.js
@@ -4,8 +4,13 @@ const router = require("./routes/index.route");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-app.use(cors({ credentials: true, origin: true }));
-
+// app.use(cors({ credentials: true, origin: true }));
+app.use(
+  cors({
+    origin: "https://attendance-api-app.vercel.app/", // Thay thế bằng URL của frontend
+    credentials: true,
+  })
+);
 //init router
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
