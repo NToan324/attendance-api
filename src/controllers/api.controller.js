ca6dfe694467a21a37af75b3afb94b3982c83519
NToan324 <nhattoan664t@gmail.com>
2024-10-21 21:44:37 +0700

    up:code

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -3,8 +3,10 @@ const form = {
   submit: async (req, res) => {
     const data = req.body;
     try {
+      console.log("Received data from control:", data);
       const result = await apiService.submit(data);
       if (result) {
+        console.log("check result:", result);
         return res.status(200).send({
           errCode: 0,
           message: "Submit form successful",
@@ -16,6 +18,7 @@ const form = {
         message: "Data is empty",
       });
     } catch (error) {
+      console.error("Error in form.submit:", error.message);
       return res.status(400).send({
         errCode: 1,
         message: "Can not submit form",
